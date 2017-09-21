1faceb0706dc246ae8b2e77f050f63561e11ac94
Robert <robert@increscent.org>
2017-09-21 00:19:33 -0600

    removed padding around some bootstrap columns

--- public/Views/Components/TransactionListItem.js
+++ public/Views/Components/TransactionListItem.js
@@ -6,13 +6,13 @@ export default function TransactionListItem(props) {
 
   return (
     <div className="row">
-      <div className="col-3">
+      <div className="col-3 no-padding">
         ${Helpers.round(transaction.amount, 2)}
       </div>
-      <div className="col-3">
+      <div className="col-3 no-padding">
         {Helpers.readableDate(transaction.date)}
       </div>
-      <div className="col-6">
+      <div className="col-6 no-padding">
         {transaction.motive}
       </div>
     </div>
--- public/Views/Overview/BudgetControls.js
+++ public/Views/Overview/BudgetControls.js
@@ -6,16 +6,16 @@ export default function BudgetControls(props) {
 
   return (
     <div className="row">
-      <div className="col-2">
+      <div className="col-2 no-padding">
         {
           budget.name != 'Other' && budget.name != 'Total' &&
           <span className="oi oi-pencil" onClick={budget.onEditBudget}></span>
         }
       </div>
-      <div className="col-5">
+      <div className="col-5 no-padding">
         Allowance: ${Helpers.round(budget.credits, 2)}
       </div>
-      <div className="col-5">
+      <div className="col-5 no-padding">
         Spent: ${Helpers.round(budget.debits, 2)}
       </div>
     </div>

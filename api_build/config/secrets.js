20582aefdd0070a124dc414271f4a676471e8a3a
Robert Williams <robert@increscent.org>
2018-01-06 19:54:24 -0700

    added padding to the footer

--- api_build/config/secrets.js
+++ api_build/config/secrets.js
@@ -3,24 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var productionSecrets = {
+  GOOGLE_API_KEY: 'AIzaSyBDInyj7t14FPxIG_TTCtErBPo25nE60G8',
+  GOOGLE_CLIENT_ID: '210769895584-4fbq6vdkp7jhnh3k930kamb67sahi447.apps.googleusercontent.com',
+  GOOGLE_CLIENT_SECRET: 'yJ-VzcziBz4n33WHp-uJ7zRr',
+  SESSION_SECRET: 'yw8kzqEKGtBFChBxKxbl',
+  GOOGLE_CALLBACK_URL: 'http://finance.increscent.org/auth/google/callback'
+};
+
 var getSecrets = exports.getSecrets = function getSecrets() {
   var environment = process.env.NODE_ENV;
   switch (environment) {
     case 'development':
-      return {
-        GOOGLE_API_KEY: 'AIzaSyBDInyj7t14FPxIG_TTCtErBPo25nE60G8',
-        GOOGLE_CLIENT_ID: '210769895584-4fbq6vdkp7jhnh3k930kamb67sahi447.apps.googleusercontent.com',
-        GOOGLE_CLIENT_SECRET: 'yJ-VzcziBz4n33WHp-uJ7zRr',
-        SESSION_SECRET: 'yw8kzqEKGtBFChBxKxbl',
+      return Object.assign({}, productionSecrets, {
         GOOGLE_CALLBACK_URL: 'http://localhost:45678/auth/google/callback'
-      };
+      });
     default:
-      return {
-        GOOGLE_API_KEY: 'AIzaSyBDInyj7t14FPxIG_TTCtErBPo25nE60G8',
-        GOOGLE_CLIENT_ID: '210769895584-4fbq6vdkp7jhnh3k930kamb67sahi447.apps.googleusercontent.com',
-        GOOGLE_CLIENT_SECRET: 'yJ-VzcziBz4n33WHp-uJ7zRr',
-        SESSION_SECRET: 'yw8kzqEKGtBFChBxKxbl',
-        GOOGLE_CALLBACK_URL: 'http://finance.increscent.org/auth/google/callback'
-      };
+      return productionSecrets;
   }
 };

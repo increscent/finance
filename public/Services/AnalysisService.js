ad4158644328f20442f00b917d6fe0a4d73f9fa1
Robert Williams <robert@increscent.org>
2018-01-06 19:40:28 -0700

    added uglifyify to build process and removed unecessary public files

--- public/Services/AnalysisService.js
+++
@@ -1,7 +0,0 @@
-import ApiService from "./ApiService.js";
-
-export default class AnalysisService {
-  fetchOverview(periodId) {
-    return ApiService.getRequest("/api/analysis/overview", periodId);
-  }
-}
